8f767ac2c193d29b8678a4a437d37961244c4647
sathya gundoji <56188753+sathya43@users.noreply.github.com>
2022-12-06 00:00:06 +0530

    Updated

--- Assignment_LAB4/Question6Sol.sql
+++ Assignment_LAB4/Question6Sol.sql
@@ -1,1 +1,4 @@
-select category.cat_id,category.cat_name,product.pro_name,supplier_pricing.supp_price from supplier_pricing join ( product join category on product.cat_id = category.cat_id) on supplier_pricing.pro_id = product.pro_id group by category.cat_id having min(supp_price);
+select category.cat_id,category.cat_name,product.pro_name,min(supplier_pricing.supp_price) from 
+   ( (supplier_pricing join product on supplier_pricing.pro_id = product.pro_id)
+     join category on product.cat_id = category.cat_id
+     ) group by category.cat_id order by category.cat_id;
